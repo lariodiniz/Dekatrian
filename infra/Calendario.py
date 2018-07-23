fdbde3fe2acded2741dbf061c8dc82b0c8df1e5d
LarioDiniz <lario.diniz@palo.com.br>
2018-07-23 14:54:56 -0300

    Nome dos meses adicionado na classe Meses

---
+++ infra/Calendario.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+
+__author__ = "Lário dos Santos Diniz"
+
+"""
+Data de Criação: 23/07/2018
+"""
+
+class Calendario():
+	"""
+	Classe básica que define o Calendario
+	"""
+	
+	def __init__(self, data):
+		self._data_base = data
+		self.
+	
---
+++ infra/meses.py
@@ -0,0 +1,26 @@
+#coding: utf-8
+
+__author__ = "Lário dos Santos Diniz"
+
+"""
+Data de Criação: 23/07/2018
+"""
+
+class Meses():
+	
+	def __init__(self):
+		self._nomes =(
+		"Auran", 
+		"Borean", 
+		"Coronian", 
+		"Driadan", 
+		"Electran", 
+		"Faian", 
+		"Gaian", 
+		"Hermetian", 
+		"Irisian", 
+		"Kaosian"
+		"Lunan",
+		"Maian",
+		"Nixian")
+	
