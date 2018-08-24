253a7d96f011cc1d5a77df5cfed362a702bf760d
LarioDiniz <lario.diniz@palo.com.br>
2018-08-24 14:47:23 -0300

    Removido CarregaKV

--- main.py
+++ main.py
@@ -10,19 +10,14 @@ from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
 from kivy.properties import ObjectProperty
 from kivy.config import Config
-from kivy.logger import Logger
+from kivy.lang import Builder
 
 from infra.view.tela_inicial import TelaInicial
 from infra.regras.data import Data
-from infra.regras.carrega_kv import CarregaKV
 
 from infra.view.area_menu import AreaMenu
 
 kivy.require('1.9.1')
-Config.set('graphics', 'resizable', 0)
-Config.set('kivy', 'window_icon', 'img/icon.png')
-Window.clearcolor = get_color_from_hex("#B0C4DE")
-Window.size = (360, 640)
 
 
 class DekatrianApp(App):
@@ -33,22 +28,40 @@ class DekatrianApp(App):
     tela = ObjectProperty(str())
 
     def __init__(self, **kwargs):
+        self._configuracaoInicial()
         super(DekatrianApp, self).__init__(**kwargs)
+        self._carregaKv()
+
+    def _configuracaoInicial(self):
+        Config.set('graphics', 'resizable', 0)
+        Config.set('kivy', 'window_icon', 'img/icon.png')
+        Window.clearcolor = get_color_from_hex("#B0C4DE")
+        Window.size = (360, 640)
+
+    def _carregaKv(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+
+        kv_path = path+'\\infra\\view\\kv'
+        kvs = self._listarArquivos(".kv", kv_path)
+
+        for kv in kvs:
+            Builder.load_file(kv_path+"\\"+kv)
+
+    def _listarArquivos(self, ext, path):
+        """Lista todos os arquivos que estão em um diretório com a extencao 
+        definida"""
+
+        caminhos = [os.path.join(path, nome) for nome in os.listdir(path)]
+        arquivos = [arq for arq in caminhos if os.path.isfile(arq)]
+        arqFiltrados = [arq for arq in arquivos if arq.lower().endswith(ext)]
+        return [arq.split('\\')[-1] for arq in arqFiltrados]   
 
     def build(self):
 
-        CarregaKV()
         self.icon = 'img/icon.png'
         self.title = 'Calendario Dekatrian'
         self.tela = 'Calendario'
         return TelaInicial()
-
-    def on_start(self):
-        Logger.info('App: Aplicação Iniciada!')
-
-    def on_stop(self):
-        Logger.info('App:  Aplicação Finalizada')
-
 
 if __name__ in ('__android__', '__main__'):
 
