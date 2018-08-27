fc0d6b899e8b0cba7d38083dbebc8827353aa6b3
LarioDiniz <lario.diniz@palo.com.br>
2018-08-27 13:00:25 -0300

    App funcionando em um programa android

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import os
 import kivy
+import platform as plataforma
 
 
 from datetime import date
@@ -11,6 +12,7 @@ from kivy.utils import get_color_from_hex
 from kivy.properties import ObjectProperty
 from kivy.config import Config
 from kivy.lang import Builder
+from kivy.utils import platform
 
 from infra.view.tela_inicial import TelaInicial
 from infra.regras.data import Data
@@ -30,31 +32,43 @@ class DekatrianApp(App):
     def __init__(self, **kwargs):
         self._configuracaoInicial()
         super(DekatrianApp, self).__init__(**kwargs)
+        print(os.name)
         self._carregaKv()
 
     def _configuracaoInicial(self):
         Config.set('graphics', 'resizable', 0)
         Config.set('kivy', 'window_icon', 'img/icon.png')
         Window.clearcolor = get_color_from_hex("#B0C4DE")
-        Window.size = (360, 640)
+        if platform != 'android':
+            Window.size = (360, 640)
 
-    def _carregaKv(self):
+    def _defineBarra(self):
+        if plataforma.system() == 'Windows':
+            return '\\'
+        else:
+            return '/'
+
+    def _carregaKv(self):        
+
+        b = self._defineBarra()
+
         path = os.path.dirname(os.path.abspath(__file__))
 
-        kv_path = path+'\\infra\\view\\kv'
+        kv_path = path+b+'infra'+b+'view'+b+'kv'
         kvs = self._listarArquivos(".kv", kv_path)
 
         for kv in kvs:
-            Builder.load_file(kv_path+"\\"+kv)
+            Builder.load_file(kv_path+b+kv)
 
     def _listarArquivos(self, ext, path):
         """Lista todos os arquivos que estão em um diretório com a extencao 
         definida"""
 
+        b = self._defineBarra()
         caminhos = [os.path.join(path, nome) for nome in os.listdir(path)]
         arquivos = [arq for arq in caminhos if os.path.isfile(arq)]
         arqFiltrados = [arq for arq in arquivos if arq.lower().endswith(ext)]
-        return [arq.split('\\')[-1] for arq in arqFiltrados]   
+        return [arq.split(b)[-1] for arq in arqFiltrados]
 
     def build(self):
 
@@ -68,5 +82,5 @@ if __name__ in ('__android__', '__main__'):
     janela = DekatrianApp()
     janela.run()
 
-__version__ = "0.0.1"
+__version__ = '0.0.1'
 __author__ = "Lário dos Santos Diniz"
