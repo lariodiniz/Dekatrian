bcc24b5bb3d763c2c00de5efa377e37272bccae9
lariodiniz <lariodiniz@gmail.com>
2019-05-30 17:53:08 -0300

    Trocada cores das telas.

--- infra/view/tela_dekatrian.py
+++ infra/view/tela_dekatrian.py
@@ -3,23 +3,19 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.utils import get_color_from_hex
 
 def Texto():
+    
     return '''
+Dekatrian e um calendario idealizado 
+pelo integrante da equipe do podcast 
+Scicast:
 
+Roberto Spinelli.
 
+Visite a pagina da Deviante ou o grupo 
+do facebook do Calendario Dekatrian 
+para saber mais.
 
-
-
-
-    Dekatrian e um calendario idealizado pelo 
-    integrante da equipe do podcast Scicast:
-
-    Roberto Spinelli.
-
-    Visite a pagina da Deviante ou o grupo do
-    facebook do Calendario Dekatrian para 
-    saber mais.
-
-    [ref=http:////www.deviante.com.br//podcasts//scicast//]www.deviante.com.br//podcasts//scicast//[/ref]
+[ref=http:////www.deviante.com.br//podcasts//scicast//]www.deviante.com.br//podcasts//scicast//[/ref]
 '''
 
 class TelaDekatrian(BoxLayout):
--- infra/view/tela_desenvolvimento.py
+++ infra/view/tela_desenvolvimento.py
@@ -5,33 +5,22 @@ from kivy.utils import get_color_from_hex
 def Texto():
 
     return '''
-
-
-
-
-
-
-
-
-
-
-Este programa esta sendo desenvolvido por
-[ref=https://twitter.com/lariodiniz]Lario Diniz[/ref]. 
-Para criticas, sugestoes e contatos envie
- um email para:
+Este programa está sendo desenvolvido 
+por [ref=https://twitter.com/lariodiniz]Lario Diniz[/ref]. Para criticas, 
+sugestoes e contatos envie um email para:
 
 lariodiniz@gmail.com
 
-Este programa e de codigo aberto e gratuito, 
-ele utiliza a licenca MIT. 
-Acesse o repositorio do codigo e contribua 
-no desenvolvimento.
+Este programa é de codigo aberto e 
+gratuito, ele utiliza a licenca MIT. 
+Acesse o repositorio do codigo e 
+contribua no desenvolvimento.
 
 [ref=https://github.com/lariodiniz/Dekatrian]github.com/lariodiniz/Dekatrian[/ref]
 
-Icones dos botões feitos por [ref=https://www.flaticon.com/authors/dinosoftlabs]DinosoftLabs[/ref] 
-para [ref=https://www.flaticon.com/]www.flaticon.com[/ref] e usa a licença 
-[ref=http://creativecommons.org/licenses/by/3.0/]CC 3.0[/ref] 
+Icones dos botões feitos por 
+[ref=https://www.flaticon.com/authors/dinosoftlabs]DinosoftLabs[/ref] para [ref=https://www.flaticon.com/]www.flaticon.com[/ref] 
+e usa a licença [ref=http://creativecommons.org/licenses/by/3.0/]CC 3.0[/ref] 
 
 
 Tecnologias utilizadas no desenvolvimento:
